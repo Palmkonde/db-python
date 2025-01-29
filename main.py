db20ef3f6d6779e8dc84c5e9b83ffab886ad3ba3
PalmPalmPalmPalmPalm <93920339+Palmkonde@users.noreply.github.com>
2025-01-29 11:46:12 +0700

    Adding data

--- main.py
+++ main.py
@@ -24,7 +24,17 @@ Games
 # con.execute("CREATE TABLE mobile_phone (id INTEGER PRIMARY KEY, model TEXT, country TEXT, OS TEXT)")
 # con.execute("CREATE TABLE mobile_game (id INTEGER PRIMARY KEY, name TEXT, publish_year SMALLINT, downloaded INT, OS TEXT)")
 
+# Insert
+# to mobile_phone
+con.execute("INSERT INTO mobile_phone (model, country, OS) VALUES ('IPhone 14', 'USA', 'IOS')")
+con.execute("INSERT INTO mobile_phone (model, country, OS) VALUES ('Samsung Galaxy S22', 'South Korea', 'Android')")
+con.execute("INSERT INTO mobile_phone (model, country, OS) VALUES ('Google Pixel 7', 'USA', 'Android')")
+con.execute("INSERT INTO mobile_phone (model, country, OS) VALUES ('Xiaomi 12', 'China', 'Android')")
+con.execute("INSERT INTO mobile_phone (model, country, OS) VALUES ('OnePlus 10 Pro', 'China', 'Android')")
 
-
-
-
+# to mobile_game
+con.execute("INSERT INTO mobile_game (name, publish_year, downloaded, OS) VALUES ('Angry Birds', 2009, 5000000, 'iOS')")
+con.execute("INSERT INTO mobile_game (name, publish_year, downloaded, OS) VALUES ('Candy Crush Saga', 2012, 10000000, 'Android')")
+con.execute("INSERT INTO mobile_game (name, publish_year, downloaded, OS) VALUES ('PUBG Mobile', 2018, 100000000, 'Android')")
+con.execute("INSERT INTO mobile_game (name, publish_year, downloaded, OS) VALUES ('Clash of Clans', 2012, 50000000, 'iOS')")
+con.execute("INSERT INTO mobile_game (name, publish_year, downloaded, OS) VALUES ('Subway Surfers', 2012, 100000000, 'Android')")
